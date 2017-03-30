e903900fc226886397e495acfe6159cde78268ae
Dan Vega <danvega@gmail.com>
2017-03-30 18:50:47 -0400

    Initial commit

---
+++ src/main/java/com/therealdanvega/User.java
@@ -0,0 +1,18 @@
+package com.therealdanvega;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class User {
+
+    private String first;
+    private String middle;
+    private String last;
+    private String email;
+    private Date dob;
+    private String foo;
+    private String bar;
+    private String a,b,c;
+
+}
